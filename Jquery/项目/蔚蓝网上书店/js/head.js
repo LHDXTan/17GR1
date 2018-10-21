e528bbd0c160b6bc15f1009611c3cdb66e509c12
xwp123 <1042797825@qq.com>
2018-10-21 18:54:29 +0800

    Signed-off-by: xwp123 <1042797825@qq.com>

--- Jquery/项目/蔚蓝网上书店/js/head.js
+++
@@ -1,9 +0,0 @@
-﻿//xiala
-$(function(){
-
-    $("#menu").mouseenter(function(){
-        $("#dd_menu_top_down").stop(true,true).slideDown(1000);
-    }).mouseleave(function(){
-        $("#dd_menu_top_down").stop(true,true).slideUp(1000);
-    });
-});
