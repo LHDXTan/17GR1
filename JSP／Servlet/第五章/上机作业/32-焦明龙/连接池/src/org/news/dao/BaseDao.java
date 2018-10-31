3131dee91e68ebfc590188e828d621bf0283ef9b
busiliangzinanwang <2960833278@qq.com>
2018-10-31 21:41:18 +0800

    Signed-off-by: busiliangzinanwang <2960833278@qq.com>焦明龙提交作业

---
+++ JSP／Servlet/第五章/上机作业/32-焦明龙/连接池/src/org/news/dao/BaseDao.java
@@ -0,0 +1,74 @@
+﻿package org.news.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class BaseDao {
+	protected Connection conn;
+	protected PreparedStatement pstmt;
+	protected ResultSet rs;
+	
+	public Connection getConnection() {
+		Context ctx;
+		// 获取连接并捕获异常
+		try {
+			ctx=new InitialContext();
+			DataSource ds=(DataSource)ctx.lookup("java:comp/env/jdbc/news");
+			conn=ds.getConnection();		
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}  
+		return conn;
+	}
+	
+	public void closeAll(Connection conn, Statement stmt, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public int exceuteUpdate(String sql,Object...prams){
+		int result=0;
+		conn=this.getConnection();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			for(int i=0;i<prams.length;i++){
+				pstmt.setObject(i+1, prams[i]);	
+			}
+			result=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			closeAll(conn, pstmt, rs);
+		}
+		return result;
+	}
+}
