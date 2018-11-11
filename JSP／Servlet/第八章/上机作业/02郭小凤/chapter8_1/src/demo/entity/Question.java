d617e171b8d34fa56bec6da6e704a3f26552c354
2486keke <2486591906@qq.com>
2018-11-11 19:19:16 +0800

    02郭小凤

---
+++ JSP／Servlet/第八章/上机作业/02郭小凤/chapter8_1/src/demo/entity/Question.java
@@ -0,0 +1,35 @@
+package demo.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Question implements Serializable {
+    private static final long serialVersionUID = -2734313491451856178L;
+    private String username;
+    private String city;
+    private List<String> languages;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+}
