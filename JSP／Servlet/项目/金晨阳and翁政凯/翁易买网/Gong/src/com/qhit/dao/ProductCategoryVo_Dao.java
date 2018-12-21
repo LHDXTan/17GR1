0ebe0c4be1b234debdf20c8a67617e2ffe587c09
MrJkim <468255939@qq.com>
2018-12-21 10:25:41 +0800

    徐文鹏Signed-off-by: MrJkim <468255939@qq.com>

--- JSP／Servlet/项目/金晨阳and翁政凯/翁易买网/Gong/src/com/qhit/dao/ProductCategoryVo_Dao.java
+++
@@ -1,9 +0,0 @@
-package com.qhit.dao;
-import java.util.List;
-import com.qhit.util.ProductCategoryVo;
-
-
-public interface ProductCategoryVo_Dao {
-	//查询商品列表
-	public List<ProductCategoryVo> selectVo_Dao();
-}
--- JSP／Servlet/项目/金晨阳and翁政凯/翁易买网/数据库脚本/easybuy/src/com/qhit/util/Test.java
+++
@@ -1,7 +0,0 @@
-package com.qhit.util;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("dsakjhdkjass");
-	}
-}
